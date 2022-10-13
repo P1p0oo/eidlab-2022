ef138dc5f950a107accfaefc6843c7354ed6731e
Ludovic <ludovic@MacBook-Pro-de-Ludovic.local>
2022-10-13 11:45:39 +0200

    Project replacement + little clean of unused files

--- pages/components/Projects.jsx
+++ pages/components/Projects.jsx
@@ -1,47 +1,54 @@
-import { Center, Flex, Heading } from "@chakra-ui/react";
+import { Center, Container, Flex, Heading } from "@chakra-ui/react";
 import Project from "./Project";
 
 const Projects = () => {
   return (
-    <Flex
-      width={"100%"}
-      color={"brand.bodyInvert"}
-      direction={"column"}
-      alignItems={"center"}
-      mb={20}
+    <Container
+      maxW={"container.xxl"}
+      pl={{ base: 0, xl: 20, "3xl": 0 }}
+      pr={{ base: 0, xl: 20, "3xl": 0 }}
     >
-      <Heading as={"h2"} size={"3xl"}>
-        Réalisations
-      </Heading>
-      <Flex direction={"column"}>
-        <Project
-          image={"/images/boiron.png"}
-          title={"Site corpo Boiron"}
-          techs={["ReactJS", "NextJS", "Drupal", "GraphQL"]}
-          link={"https://www.boiron.fr/"}
-        ></Project>
-        <Project
-          image={"/images/meunier.png"}
-          title={"Distillerie Meunier"}
-          techs={["Wordpress", "Javascript", "Drupal"]}
-          link={"https://www.boiron.fr/"}
-          revert={true}
-        ></Project>
-        <Project
-          image={"/images/lespipelettes.png"}
-          title={"Les Pipelettes"}
-          techs={["ReactJS", "NodeJS", "Websocket", "Symfony"]}
-          link={"https://www.boiron.fr/"}
-        ></Project>
-        <Project
-          image={"/images/nouveaumonde.png"}
-          title={"Nouveau Monde"}
-          techs={["ReactJS", "Gatsby", "Wordpress", "GraphQL"]}
-          link={"https://www.boiron.fr/"}
-          revert={true}
-        ></Project>
+      <Flex
+        width={"100%"}
+        color={"brand.bodyInvert"}
+        direction={"column"}
+        alignItems={"space-between"}
+        mb={20}
+      >
+        <Heading as={"h2"} size={"3xl"} textAlign={"center"}>
+          Réalisations
+        </Heading>
+        <Flex direction={"column"}>
+          <Project
+            image={"/images/inside-lyon.png"}
+            title={"Inside Lyon"}
+            techs={["Wordpress", "Javascript"]}
+            link={"https://www.inside-lyon.com/"}
+          ></Project>
+          <Project
+            image={"/images/meunier.png"}
+            title={"Distillerie Meunier"}
+            techs={["Wordpress", "Javascript", "Drupal"]}
+            link={"https://www.distillerie-meunier.fr/"}
+            revert={true}
+          ></Project>
+          <Project
+            image={"/images/boiron.png"}
+            title={"Site corpo Boiron"}
+            techs={["ReactJS", "NextJS", "Drupal", "GraphQL"]}
+            link={"https://www.boiron.fr/"}
+          ></Project>
+
+          <Project
+            image={"/images/nouveaumonde.png"}
+            title={"Nouveau Monde"}
+            techs={["ReactJS", "Gatsby", "Wordpress", "GraphQL"]}
+            link={"https://www.nouveaumonde.fr/"}
+            revert={true}
+          ></Project>
+        </Flex>
       </Flex>
-    </Flex>
+    </Container>
   );
 };
 
